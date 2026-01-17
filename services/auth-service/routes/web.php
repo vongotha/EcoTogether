5f8faa12a309a814758c9c5a0bb02a3362bb28c8
Argon <vongotha0@gmail.com>
2026-01-17 21:35:30 +0100

    Initial Jenkins For Build

--- services/auth-service/routes/web.php
+++ services/auth-service/routes/web.php
@@ -5,13 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Ceci est le service Auth v2!";
 });
-
-/*
-je cherche a faire une nuance... why routes de auth service ne fonctionnne nada... ex si j'essaie d'acceder a 
-
-Route::get('/', function () {
-
-    return view('welcome');
-
-}); http://eco-post.local/auth/ j'ai just la page laravel standard mais aussi.... le auth service avec sanctum et tout ca va servir a quoi
-*/
